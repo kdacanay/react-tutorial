aece82c0cae00720ffe409c6ae3a5e4adee30606
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-07-09 01:36:32 -0400

    Updates

---
+++ precache-manifest.0ba0e7f47d7b4d9a2a1698d18dd9c38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bf8133926445db09c321e667a3dde2",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "d635e96a0d3cca693a16",
+    "url": "/react-tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d635e96a0d3cca693a16",
+    "url": "/react-tutorial/static/js/main.a92fff0a.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
